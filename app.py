ce917cb25421ec172c1078646c971088790bd4c2
SergioNinho <ninho0326@gmail.com>
2024-12-05 18:49:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ def envia_x():
         return render_template('descarga_x.html', mensaje_2 = mensaje_2) 
         
 if __name__=='__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0')
